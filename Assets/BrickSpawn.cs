b9c0b0193579c4ba8afb59abf7d7600cff078414
immersvin <immersv@gmail.com>
2021-02-22 21:17:56 +0530

    Brick Beaker Game Intialized and Random Super brick Generated

---
+++ Assets/BrickSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickSpawn : MonoBehaviour
+{
+    public GameObject brick;
+    public float offset;
+    public GameObject[] brickArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        brickArray = new GameObject[7];
+        for(int i = 0; i < brickArray.Length; i++)
+        {
+           transform.position = new Vector3(transform.position.x+2.0f, 10.0f, 0);
+           brickArray[i]= Instantiate(brick,  transform.position, Quaternion.identity);
+            
+        }
+        int randValue = Random.Range(1, brickArray.Length);
+        brickArray[randValue].tag = "SuperBrick";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PaddleMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    [SerializeField]
+    float screenWidthInUnits = 16.0f;
+    [SerializeField]
+    float minX,maxX;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseInputPos = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mouseInputPos, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
